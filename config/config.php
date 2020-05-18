0268c831c872fad8249eff81428baf9e23292656
Thomas Sihapanya <sihapanya.thomas@gmail.com>
2020-05-18 10:30:43 +0200

    :sparkle: Require config into index

---
+++ config/config.php
@@ -0,0 +1,5 @@
+<?php
+
+require __DIR__ . '/../src/models/Animal.php';
+require __DIR__ . '/../src/models/AnimalZoo.php';
+require __DIR__ . '/../src/models/Zoo.php';
---
+++ index.php
@@ -0,0 +1,6 @@
+<?php
+
+require __DIR__ . '/config/config.php';
+
+$simba = new Animal;
+var_dump($simba);
